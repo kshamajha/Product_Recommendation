5631b9ae84021ce87ee958ee178a608217a58f1e
Kshama Jha <kshama0602@gmail.com>
2021-11-15 01:14:51 +0800

    Refactor code and updated jupyter file

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# Final Sentiment and Recommendation Model with the code to deploy the end-to-end project using Flask and Heroku
+# Final Sentiment Based Product Recommendation Model with the code to deploy the end-to-end project using Flask and
+# Heroku
 
 # Suppress Warnings
 import warnings
@@ -14,7 +15,7 @@ import pickle as pkl
 # import all pickle files
 # best_model.pkl - sentiment analysis best model pickle file
 # tfidf.pkl - tfidf vectorizer
-# transform.pkl - this pickle file after text cleaning
+# transform.pkl - the pickle file after text cleaning
 # user_recommendation.pkl - user based recommendation model
 
 model = pkl.load(open('models/best_model.pkl', 'rb'))
@@ -23,6 +24,18 @@ transform = pkl.load(open('dataset/transform.pkl', 'rb'))
 user_recom = pkl.load(open('models/user_recommendation.pkl', 'rb'))
 
 
+# Function to get the top 20 products based on user recommendation
+def recommendation(user_input):
+    try:
+        result = True
+        recom_data = user_recom.loc[user_input].sort_values(ascending=False)[0:20].index
+    except:
+        result = False
+        recom_data = "User with username \"" + user_input + "\" not found, try another username"
+    return result, recom_data
+
+
+# Function to get the top 5 products from the recommended 20 products
 def sentiment(recom_prod):
     df = transform[transform.name.isin(recom_prod)]
     features = tfidf.transform(df['text'])
@@ -35,13 +48,3 @@ def sentiment(recom_prod):
     df['Product Name'] = df.index
     output_data = df[['Product Name']][:5].reset_index(drop=True)
     return output_data
-
-
-def recommendation(user_input):
-    try:
-        result = True
-        recom_data = user_recom.loc[user_input].sort_values(ascending=False)[0:20].index
-    except:
-        result = False
-        recom_data = "User with username \"" + user_input + "\" not found, try another username"
-    return result, recom_data
